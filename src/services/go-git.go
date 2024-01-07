2818620aaa4d04d1caccb068a92c72e944384294
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-01-07 14:23:02 -0800

    Added get last commit hash (#3)

--- src/services/go-git.go
+++ src/services/go-git.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
@@ -11,36 +12,36 @@ import (
 type GoGitService struct{}
 
 func (gs *GoGitService) CheckoutAndCollectLastCommitDate(repoURL string, token string) (time.Time, error) {
-	r, err := getRepository(repoURL, token)
-
-	// Get the HEAD reference
-	ref, err := r.Head()
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	// Get the commit object from the reference
-	c, err := r.CommitObject(ref.Hash())
-	if err != nil {
-		return time.Time{}, err
-	}
+	c, _ := getCommitObject(repoURL, token)
 
 	// Return the date of the commit
 	return c.Author.When, nil
 }
 
+func (gs *GoGitService) CheckoutAndCollectLastCommitHash(repoURL string, token string) (string, error) {
+	c, _ := getCommitObject(repoURL, token)
+
+	// Return the date of the commit
+	return c.Hash.String(), nil
+}
+
+func getCommitObject(repoURL string, token string) (*object.Commit, error) {
+	r, _ := getRepository(repoURL, token)
+
+	// Get the HEAD reference
+	ref, _ := r.Head()
+
+	// Get the commit object from the reference
+	return r.CommitObject(ref.Hash())
+}
+
 func getRepository(repoURL string, token string) (*git.Repository, error) {
 	// Create a new repository object with the given URL
-	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+	return git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
 		URL: repoURL,
 		Auth: &http.BasicAuth{
 			Username: "git-user",
 			Password: token,
 		},
 	})
-	if err != nil {
-		return nil, err
-	}
-
-	return r, nil
 }
